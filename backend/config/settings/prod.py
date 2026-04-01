8e5e0fc57a511b3da02fdcbf6eddfd311063259d
Sug <sugar77.xe@gmail.com>
2026-04-01 20:22:18 +0900

    fix: prod.pyのipアドレスをurlに

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -8,3 +8,5 @@ CORS_ALLOWED_ORIGINS = [os.environ.get("ALLOWED_ORIGINS_URL", "")]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS_URL", ""), "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [os.environ.get("ALLOWED_ORIGINS_URL", "")]
